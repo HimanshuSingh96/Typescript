2fc71ab28b31560f80b0716891869ea5bbcb04eb
Himanshu Singh <58973079+HimanshuSingh96@users.noreply.github.com>
2020-02-07 16:31:55 +0530

    Update demo.ts

--- demo.ts
+++ demo.ts
@@ -5,7 +5,7 @@ console.log(message);
 var num:number = 12 
 console.log(num)
 
-
+//classes and objects
 class Greeting { 
    greet():void { 
       console.log("Hello World!!!") 
@@ -14,6 +14,7 @@ class Greeting {
 var obj = new Greeting(); 
 obj.greet();
 
+//variables
 var name:string = "John"; 
 var score1:number = 50;
 var score2:number = 42.50
@@ -22,3 +23,18 @@ console.log("name"+name)
 console.log("first score: "+score1) 
 console.log("second score: "+score2) 
 console.log("sum of the scores: "+sum)
+
+//scopes
+var global_num = 12          //global variable 
+class Numbers { 
+   num_val = 13;             //class variable 
+   static sval = 10;         //static field 
+   
+   storeNum():void { 
+      var local_num = 14;    //local variable 
+   } 
+} 
+console.log("Global num: "+global_num)  
+console.log(Numbers.sval)   //static variable  
+var obj = new Numbers(); 
+console.log("Global num: "+obj.num_val)
