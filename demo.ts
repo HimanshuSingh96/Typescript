be53d6e51c5595f3f1683d96b7c03987e6281163
Himanshu Singh <58973079+HimanshuSingh96@users.noreply.github.com>
2020-02-07 16:29:05 +0530

    demo.ts

---
+++ demo.ts
@@ -0,0 +1,24 @@
+const message: string = 'hello world';
+console.log(message);
+
+
+var num:number = 12 
+console.log(num)
+
+
+class Greeting { 
+   greet():void { 
+      console.log("Hello World!!!") 
+   } 
+} 
+var obj = new Greeting(); 
+obj.greet();
+
+var name:string = "John"; 
+var score1:number = 50;
+var score2:number = 42.50
+var sum = score1 + score2 
+console.log("name"+name) 
+console.log("first score: "+score1) 
+console.log("second score: "+score2) 
+console.log("sum of the scores: "+sum)
